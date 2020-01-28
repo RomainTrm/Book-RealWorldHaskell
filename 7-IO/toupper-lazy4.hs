ef7bc298159406180b39755ef55ed40889190da1
DESKTOP-5V2OR16\Romain <you@example.com>
2020-01-28 15:01:09 +0100

    I/O code examples

---
+++ 7-IO/toupper-lazy4.hs
@@ -0,0 +1,12 @@
+import Data.Char(toUpper)
+
+main = interact (map toUpper)
+
+-- Write on a file
+-- $ runghc toupper-lazy4.hs < input.txt > output.txt
+
+-- Display on screen
+-- $ runghc toupper-lazy4.hs < input.txt
+
+-- Echo every input on terminal
+-- $ runghc toupper-lazy4.hs
